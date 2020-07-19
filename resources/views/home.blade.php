59c6ac938765c34219a67e7c15f7c186e6b74a39
JaimeGarcia94 <jaimegarciagar94@gmail.com>
2020-07-19 16:30:43 +0200

    refactor code js in new file in directory public js

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,11 +9,11 @@
                     <div class="row ">
                         <div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
                             <div class="container-card-panel card bg-dark">
-                                <a href="#" class="link-panel-game text-white" data-toggle="popover" data-trigger="hover" data-content="Some content">
+                                <a href="#" class="link-panel-game text-white" >
                                     <img class="card-img" src="{{ url('/images/not-user.jpg') }}" alt="Card image">
                                     <div class="card-img-overlay">
                                         <div class="description-card-game">
-                                            <h5 class="card-title">Easy mode</h5>
+                                            <h5 class="card-title" data-toggle="popover" data-placement="bottom" data-trigger="hover" data-content="Palabras cada 10 segundos.">Easy mode</h5>
 {{--                                            <a href="" class="card-title" data-toggle="popover" data-trigger="hover" data-content="Some content">Card title</a>--}}
                                         </div>
                                     </div>
@@ -28,7 +28,6 @@
                                     <div class="card-img-overlay">
                                         <div class="description-card-game">
                                             <h5 class="card-title">Card title</h5>
-                                            <p class="card-text">Last updated 3 mins ago</p>
                                         </div>
                                     </div>
                                 </a>
@@ -42,7 +41,6 @@
                                     <div class="card-img-overlay">
                                         <div class="description-card-game">
                                             <h5 class="card-title">Card title</h5>
-                                            <p class="card-text">Last updated 3 mins ago</p>
                                         </div>
                                     </div>
                                 </a>
@@ -59,7 +57,6 @@
                                     <div class="card-img-overlay">
                                         <div class="description-card-game">
                                             <h5 class="card-title">Card title</h5>
-                                            <p class="card-text">Last updated 3 mins ago</p>
                                         </div>
                                     </div>
                                 </a>
@@ -73,7 +70,6 @@
                                     <div class="card-img-overlay">
                                         <div class="description-card-game">
                                             <h5 class="card-title">Card title</h5>
-                                            <p class="card-text">Last updated 3 mins ago</p>
                                         </div>
                                     </div>
                                 </a>
@@ -87,7 +83,6 @@
                                     <div class="card-img-overlay">
                                         <div class="description-card-game">
                                             <h5 class="card-title">Card title</h5>
-                                            <p class="card-text">Last updated 3 mins ago</p>
                                         </div>
                                     </div>
                                 </a>
@@ -99,12 +94,6 @@
                 </div>
             </div>
         </div>
-    </div>
-
-    <div class="container">
-        <h3>Popover Example</h3>
-        <a href="#" title="Header" data-toggle="popover" data-content="Some content">Click Me</a><br>
-        <a href="#" title="Header" data-toggle="popover" data-trigger="hover" data-content="Some content">Hover over me</a>
     </div>
 @endsection
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/popoverPanel.js') }}" defer></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
@@ -95,10 +96,5 @@
             @yield('content')
         </main>
     </div>
-    <script>
-        $(document).ready(function(){
-            $('[data-toggle="popover"]').popover();
-        });
-    </script>
 </body>
 </html>
