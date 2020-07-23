1186174fe0eba0ac9086063f8d37bb8161b802be
JaimeGarcia94 <jaimegarciagar94@gmail.com>
2020-07-23 18:57:04 +0200

    get extract data BD words

--- app/Http/Controllers/EasyModeController.php
+++ app/Http/Controllers/EasyModeController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Word;
 
 class EasyModeController extends Controller
 {
     public function index()
     {
+        $this->getWords();
         return view('easymode.content');
 
     }
+
+    public function getWords()
+    {
+        $logs = Word::all();
+
+        return $logs;
+    }
+
 }
--- resources/views/easymode/content.blade.php
+++ resources/views/easymode/content.blade.php
@@ -1,7 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-
+<h1>WORDS</h1>
+{{--    @foreach($logs as $log)--}}
+{{--        <p>{{ $log }}</p>--}}
+{{--    @endforeach--}}
 @endsection
 
 
