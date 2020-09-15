46cb40567ec9e5e49bf1349218acbe2889adb424
JaimeGarcia94 <jaimegarciagar94@gmail.com>
2020-09-15 12:50:09 +0200

    create file include player

--- resources/views/easymode/content.blade.php
+++ resources/views/easymode/content.blade.php
@@ -19,4 +19,34 @@
         return listwords[randomWords];
     }
 
+
+    function createRouteBeat() {
+        var selectBeat = document.getElementById("select").value;
+        var route = "{{route('beat.show', ['filename' => 'selectBeat'])}}".replace("selectBeat", selectBeat);
+        var reproductor = document.getElementById("reproductor");
+        reproductor.setAttribute("src", route);
+        document.getElementById("myAudio").appendChild(reproductor);
+    }
+
+    function deleteRouteBeat() {
+        var buttonChangeAudio = document.getElementById("buttonAudio");
+        var stopAudio = document.getElementById("myAudio");
+        stopAudio.load();
+        buttonChangeAudio.innerHTML = "Play";
+        document.getElementsByTagName("source")[0].removeAttribute("src");
+    }
+
+    function playAudio() {
+        var audio = document.getElementById("myAudio");
+        var buttonChangeAudio = document.getElementById("buttonAudio");
+        createRouteBeat();
+        if(audio.paused){
+            audio.play();
+            buttonChangeAudio.innerHTML = "Pause";
+        }else{
+            audio.pause();
+            buttonChangeAudio.innerHTML = "Play";
+        }
+    }
+
 </script>
