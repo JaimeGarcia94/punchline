7edaed02cf11e54a1f6dcc28f319f8629b570d2f
JaimeGarcia94 <jaimegarciagar94@gmail.com>
2020-10-07 17:04:29 +0200

    refactor code audio script

--- resources/views/includes/player.blade.php
+++ resources/views/includes/player.blade.php
@@ -14,3 +14,35 @@
 
 <button onclick="playAudio()" id="buttonAudio" type="button">Play</button>
 
+@push('script')
+<script>
+    function createRouteBeat() {
+        var selectBeat = document.getElementById("select").value;
+        var route = "{{route('beat.show', ['filename' => 'selectBeat'])}}".replace("selectBeat", selectBeat);
+        var reproductor = document.getElementById("reproductor");
+        reproductor.setAttribute("src", route);
+        document.getElementById("myAudio").appendChild(reproductor);
+    }
+
+    function deleteRouteBeat() {
+        var buttonChangeAudio = document.getElementById("buttonAudio");
+        var stopAudio = document.getElementById("myAudio");
+        stopAudio.load();
+        buttonChangeAudio.innerHTML = "Play";
+        document.getElementsByTagName("source")[0].removeAttribute("src");
+    }
+
+    function playAudio() {
+        var audio = document.getElementById("myAudio");
+        var buttonChangeAudio = document.getElementById("buttonAudio");
+        createRouteBeat();
+        if(audio.paused){
+            audio.play();
+            buttonChangeAudio.innerHTML = "Pause";
+        }else{
+            audio.pause();
+            buttonChangeAudio.innerHTML = "Play";
+        }
+    }
+</script>
+@endpush
