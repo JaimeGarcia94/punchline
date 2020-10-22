8d5d830337a08513e70256c72805f4808b89da74
JaimeGarcia94 <jaimegarciagar94@gmail.com>
2020-10-22 14:29:29 +0200

    add styles in rest games

--- resources/views/easymode/content.blade.php
+++ resources/views/easymode/content.blade.php
@@ -1,20 +1,61 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 id="word" class="text-center"></h1>
-    @include('includes.player')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="box-game">
+                    <h1 class="title">Easy Mode</h1>
+                    <div class="content-box" id="content-box">
+                        <h1 id="word" class="text-center"></h1>
+                        <h1 id="secondWord" class="text-center"></h1>
+                    </div>
+                    <div class="col-md-12">
+                        @include('includes.player')
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 @stack('script')
 <script>
     var listWords = @json($words);
 
+    function showMessageInit() {
+        var contentBox = document.getElementById("content-box");
+        if (contentBox.style.visibility === "hidden") {
+            contentBox.style.display = "block";
+        } else {
+            document.getElementsByClassName("content-box")[0].removeAttribute("id");
+        }
+    }
+
+    setTimeout (function() {
+        var words = document.getElementById("word");
+        words.innerHTML = getRandomWord();
+
+        var secondWord = document.getElementById("secondWord");
+        secondWord.innerHTML = getSecondRandomWord();
+
+        showMessageInit();
+    },10000);
+
     setInterval(function () {
         var words = document.getElementById("word");
         words.innerHTML = getRandomWord();
+
+        var secondWord = document.getElementById("secondWord");
+        secondWord.innerHTML = getSecondRandomWord();
     }, 10000);
 
     function getRandomWord(){
-        const randomWords = Math.floor(Math.random()*(listWords.length ));
+        const randomWords = Math.floor(Math.random()*(listWords.length));
         return listWords[randomWords];
+    }
+
+    function getSecondRandomWord(){
+        const secondRandomWords = Math.floor(Math.random()*(listWords.length));
+        return listWords[secondRandomWords];
     }
 </script>
--- resources/views/imagesMode/content.blade.php
+++ resources/views/imagesMode/content.blade.php
@@ -6,7 +6,9 @@
             <div class="col-md-12">
                 <div class="box-game">
                     <h1 class="title">Images Mode</h1>
-                    <img id="images" class="text-center">
+                    <div class="content-box" id="content-box">
+                        <img id="images" class="text-center">
+                    </div>
                     <div class="col-md-12">
                         @include('includes.player')
                     </div>
@@ -19,6 +21,23 @@
 @stack('script')
 <script>
     var listImages = @json($images);
+
+    function showMessageInit() {
+        var contentBox = document.getElementById("content-box");
+        if (contentBox.style.visibility === "hidden") {
+            contentBox.style.display = "block";
+        } else {
+            document.getElementsByClassName("content-box")[0].removeAttribute("id");
+        }
+    }
+
+    setTimeout (function() {
+        var images = document.getElementById("images");
+        images.src = getRandomImage();
+
+
+        showMessageInit();
+    },10000);
 
     setInterval(function () {
         var images = document.getElementById("images");
