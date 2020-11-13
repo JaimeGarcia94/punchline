e25b32245f7eab900ba883a25ad8596731358504
JaimeGarcia94 <jaimegarciagar94@gmail.com>
2020-11-13 13:39:10 +0100

    edit text message reset password email

--- app/Notifications/PasswordReset.php
+++ app/Notifications/PasswordReset.php
@@ -71,13 +71,13 @@ class PasswordReset extends Notification
                 'email' => $notifiable->getEmailForPasswordReset(),
             ], false));
         }
-
+       
         return (new MailMessage)
             ->subject(Lang::get('Punchline'))
-            ->line(Lang::get('You are receiving this email because we received a password reset request for your account.'))
-            ->action(Lang::get('Reset Password'), $url)
-            ->line(Lang::get('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
-            ->line(Lang::get('If you did not request a password reset, no further action is required.'));
+            ->line(Lang::get('Estás recibiendo este correo electrónico porque recibimos una solicitud de restablecimiento de contraseña para tu cuenta..'))
+            ->action(Lang::get('Restablece la contraseña'), $url)
+            ->line(Lang::get('Este enlace de restablecimiento de contraseña caducará en :count minutos.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
+            ->line(Lang::get('Si no solicitó un restablecimiento de contraseña, no es necesario realizar ninguna otra acción.'));
     }
 
     /**
