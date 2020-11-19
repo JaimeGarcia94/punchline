5622901f57e3e45bc8d498839583adcbb1795bd9
JaimeGarcia94 <jaimegarciagar94@gmail.com>
2020-11-19 13:41:15 +0100

    style content history

--- resources/views/historyMode/content.blade.php
+++ resources/views/historyMode/content.blade.php
@@ -5,11 +5,25 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="box-game">
-                    <h1 class="title">History Mode</h1>
-                    <div class="content-box" id="content-box">
-                        <h1 id="history" class="text-center"></h1>
+
+                    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+                        <div class="container">
+                            <span class="title-game">History Mode</span>
+                            <button onclick="returnBack()" id="buttonBack">
+                                <i class="fa fa-sign-out fa-2x"></i>
+                            </button>
+                        </div>
+                    </nav>
+
+                    <div class="content-box content-box-fixed" id="content-box-hidden">
+                        <div class="content-box-body">
+                            <h1 id="history" class="text-center history-hidden"></h1>
+                        </div>
+                        <div class="span-info-game" id="span-info-game-hidden">
+                            <span class="title-game-span">History Mode | Hist. 60S</span>
+                        </div>
                     </div>
-                    <div class="col-md-12">
+                    <div class="col-md-12 content-box-player">
                         @include('includes.player')
                     </div>
                 </div>
@@ -22,11 +36,17 @@
     var listHistories = @json($histories);
 
     function showMessageInit() {
-        var contentBox = document.getElementById("content-box");
-        if (contentBox.style.visibility === "hidden") {
+        var contentBox = document.getElementById("content-box-hidden");
+        var spanInfoGame = document.getElementById("span-info-game-hidden");
+        var backgroundHistory = document.getElementById("history");
+        if (contentBox.style.visibility === "hidden" && spanInfoGame.style.visibility === "hidden" && backgroundHistory.style.visibility === "hidden") {
             contentBox.style.display = "block";
+            spanInfoGame.style.display = "block";
+            backgroundHistory.style.display = "block";
         } else {
             document.getElementsByClassName("content-box")[0].removeAttribute("id");
+            document.getElementsByClassName("span-info-game")[0].removeAttribute("id");
+            backgroundHistory.classList.remove("history-hidden");
         }
     }
 
@@ -45,5 +65,10 @@
     function getRandomHistories(){
         const randomHistories = Math.floor(Math.random()*(listHistories.length));
         return listHistories[randomHistories];
+    }
+
+    function returnBack()
+    {
+        window.location = "http://localhost:8080/home";
     }
 </script>
